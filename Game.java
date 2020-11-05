2f3d86156a7797c47d15ea6b0074172b5e58c29a
decduck3 <decoftheduck@gmail.com>
2020-11-06 08:01:29 +1100

    Started Implementing Aparapi

--- Game.java
+++ Game.java
@@ -1,7 +1,12 @@
 import java.awt.Color;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+
+import com.aparapi.*;
 
 import engine.GameEngine;
 import engine.rendering.GameRenderer;
@@ -38,14 +43,36 @@ class Game{
         }
         return new FramePart(results);
     }
+    public static <T> List<T> fromArrayToList(T[] a) { 
+	    return Arrays.stream(a).collect(Collectors.toList());
+	}
 
     public static FramePart createCube(){
-        List<Pixel> results = new ArrayList<Pixel>();
-        for(int x = 0; x < new Random().nextInt(engine.renderer.getWidth()); x++){
-            for(int y = 0; y < new Random().nextInt(engine.renderer.getHeight());y++){
-                results.add(new Pixel(x, y,  Color.BLUE));
+        final Pixel[] results = new Pixel[0];
+        final int xLimit = new Random().nextInt(engine.renderer.getWidth());
+        final int yLimit = new Random().nextInt(engine.renderer.getHeight());
+        final Pixel pixel = new Pixel(0, 0,  Color.BLUE);
+        final int[] x = new int[1];
+        final int[] y = new int[1];
+        Kernel kernel = new Kernel() {
+            @Override
+            public void run() {
+                while(x[0] < xLimit){
+                    while(y[0] < yLimit){
+                        pixel.x = x[0];
+                        pixel.y = y[0];
+                        results[results.length] = pixel;
+                        x[0] += 1;
+                        y[0] += 1;
+                    }
+                }
             }
-        }
-        return new FramePart(results);
+        };
+
+        Range range = Range.create(xLimit * yLimit);
+
+        kernel.execute(range);
+
+        return new FramePart(fromArrayToList(results));
     }
 }
