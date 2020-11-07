dc254672b373233804ca215a34db7e1330d41ba4
decduck3 <decoftheduck@gmail.com>
2020-11-07 12:01:47 +1100

    Update Game.java

--- Game.java
+++ Game.java
@@ -26,7 +26,6 @@ class Game{
         while(isRunning){
 
             engine.renderer.AddRender(background);
-            engine.renderer.AddRender(createCube());
             engine.renderer.PushRender();
 
             isRunning = engine.renderer.frame != null;
@@ -46,33 +45,4 @@ class Game{
     public static <T> List<T> fromArrayToList(T[] a) { 
 	    return Arrays.stream(a).collect(Collectors.toList());
 	}
-
-    public static FramePart createCube(){
-        final Pixel[] results = new Pixel[0];
-        final int xLimit = new Random().nextInt(engine.renderer.getWidth());
-        final int yLimit = new Random().nextInt(engine.renderer.getHeight());
-        final Pixel pixel = new Pixel(0, 0,  Color.BLUE);
-        final int[] x = new int[1];
-        final int[] y = new int[1];
-        Kernel kernel = new Kernel() {
-            @Override
-            public void run() {
-                while(x[0] < xLimit){
-                    while(y[0] < yLimit){
-                        pixel.x = x[0];
-                        pixel.y = y[0];
-                        results[results.length] = pixel;
-                        x[0] += 1;
-                        y[0] += 1;
-                    }
-                }
-            }
-        };
-
-        Range range = Range.create(xLimit * yLimit);
-
-        kernel.execute(range);
-
-        return new FramePart(fromArrayToList(results));
-    }
 }
